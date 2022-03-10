007d1434d3437a8d8a5d6d79452bdac1fb07065b
EC2 Default User <ec2-user@ip-172-31-22-150.ec2.internal>
2022-03-10 09:27:05 +0000

    second

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,13 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all.sort {|a,b|
-     b.favorites.count <=>
-     a.favorites.count }
+     to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:week_favorites).
+      sort {|a,b|
+        b.week_favorites.includes(:favorites).where(created_at: from...to).size <=>
+        a.week_favorites.includes(:favorites).where(created_at: from...to).size
+      }
     @book = Book.new
   end
 
